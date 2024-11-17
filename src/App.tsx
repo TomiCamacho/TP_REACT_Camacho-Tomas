a3303c44e845dd708737c474a2919ec7884a1d8d
tomy <tomycamacho3004@gmail.com>
2024-11-17 18:34:28 -0300

    proyecto React finalizado

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { AppProduct } from "./components/AppProduct/AppProducto";
+
+export const App = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "2vh" }}>
+      {/* <MIPrimerComponent
+        text={"Texto desde propiedades"}
+        color="red"
+        fontSize={5}
+      />
+      <ComponentCounter />
+      <ComponentUseEffect /> */}
+      {/* <FormComponent /> */}
+      <AppProduct />
+    </div>
+  );
+};
